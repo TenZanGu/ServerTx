83bc601a380921bfadb3ab0b144b7835d35e62d3
lobsang <lobsang1t2@gmail.com>
2020-02-08 20:27:07 -0500

    Contract ABI and Addr

---
+++ contract/Tipjar.js
@@ -0,0 +1,65 @@
+const CONSTRACT_ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newNum",
+				"type": "address"
+			}
+		],
+		"name": "changeOwner",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "withdrawAll",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const CONSTRACT_ADDRESS = "0x4f71b167127D674F283acE65c72F436bf46e69b2";
+
+module.exports = {
+    CONSTRACT_ABI,
+    CONSTRACT_ADDRESS
+}
